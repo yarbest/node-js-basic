12fe5509603995e8e67b95162c8b11260d9505dd
yarbest <yarbest15@gmail.com>
2025-01-11 15:14:26 +0200

    connect to MongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,35 @@
 import dotenv from 'dotenv';
+const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
+dotenv.config({ path: envFile });
+
+import mongoose from 'mongoose';
+
 import { Application } from "./application";
 import middlewareParseJson from "./middlewares/middlewareParseJson";
 import parseUrl from "./middlewares/middlewareParseUrl";
 import { Path } from "./router";
 import userRouter from "./users/user-router";
 
-dotenv.config();
 const app = new Application();
 
 app.addMiddleWare(middlewareParseJson);
 app.addMiddleWare(parseUrl(process.env.BASE_URL || 'http://localhost:3000'));
 app.addRouter(userRouter);
 
-app.listen(3000, () => {
-  console.log('Server has been started on port 3000');
-})
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL || '', {});
+    app.listen(3000, () => {
+      console.log('Server has been started on port 3000');
+    })
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+start()
+
 
 declare module 'http' {
   interface ServerResponse {
